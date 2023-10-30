a2975efe9b9feaff07fa0c2838ef373b08d06790
Ahmaad Ansari <88805493+ahmaad-ansari@users.noreply.github.com>
2023-10-29 23:57:07 -0400

    Commit.

--- app/src/main/java/com/example/carmarketplaceapplication/RegisterActivity.java
+++ app/src/main/java/com/example/carmarketplaceapplication/RegisterActivity.java
@@ -13,27 +13,27 @@ import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         setupSignupPrompt();
     }
 
     private void setupSignupPrompt() {
-        TextView signupText = findViewById(R.id.register_text);
+        TextView signupText = findViewById(R.id.login_text);
 
-        SpannableString spannable = new SpannableString("Don't have an account? Register now!");
+        SpannableString spannable = new SpannableString("Already have an account? Login now!");
 
         // Color for the "Sign up now!" part
         int linkColor = ContextCompat.getColor(this, R.color.link_text);
         spannable.setSpan(
                 new ForegroundColorSpan(linkColor),
-                23,  // start index
-                36,  // end index
+                25,  // start index
+                35,  // end index
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         );
 
@@ -42,13 +42,12 @@ public class LoginActivity extends AppCompatActivity {
                 new ClickableSpan() {
                     @Override
                     public void onClick(View widget) {
-                        // 'LoginActivity.this' is used here to refer to the LoginActivity context
-                        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                         startActivity(intent);
                     }
                 },
-                23,  // start index
-                36,  // end index
+                25,  // start index
+                35,  // end index
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
         );
 
